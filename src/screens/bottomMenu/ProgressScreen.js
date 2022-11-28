a4576d909e9a09e0065fda76347353911ac4cc2c
Tien Hoc <hocntgch190844@fpt.edu.vn>
2022-11-28 17:12:24 +0700

    update progress

--- src/screens/bottomMenu/ProgressScreen.js
+++ src/screens/bottomMenu/ProgressScreen.js
@@ -1,5 +1,5 @@
-import { View, Dimensions, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Dimensions, TouchableOpacity, Modal, Pressable, TextInput } from 'react-native'
+import React, { useState } from 'react'
 import style from '../../styles/tabsStyle'
 import HeaderBar from '../../components/HeaderBar'
 import { LineChart } from 'react-native-chart-kit'
@@ -7,9 +7,19 @@ import AppText from '../../components/AppText'
 import color from '../../styles/color'
 import CircularProgress from 'react-native-circular-progress-indicator'
 
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
 const ProgressScreen = () => {
 
-  const data = [55, 56, 57, 56.5, 58, 60, 80]
+  const [goalWeight, setGoalWeight] = useState(70)
+  const [currentWeight, setCurrentWeight] = useState(60)
+  const [newWeight, setNewWeight] = useState()
+  const [isVisible, setIsVisible] = useState(false)
+
+  const [dataChart, setDataChart] = useState([55, 56, 57, 56.5, 58, 60])
+  const [labelChart, setLabelChart] = useState(
+    ["Jan", "Feb", "Mar", "Apr", "May", "June"]
+  )
 
   const chartConfig = {
     backgroundGradientFrom: "white",
@@ -17,6 +27,30 @@ const ProgressScreen = () => {
     decimalPlaces: 1,
     color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
     labelColor: () => `grey`,
+  }
+
+  const updateWeight = () => {
+
+    const d = new Date();
+    let date = d.getDate()
+    let month = d.getMonth() + 1
+
+    if (newWeight) {
+      setCurrentWeight(newWeight)
+      setDataChart([
+        ...dataChart,
+        newWeight
+      ])
+
+      setLabelChart([
+        ...labelChart,
+        `${date}/${month}`
+      ])
+
+      setIsVisible(false)
+    } else {
+      alert('Please enter new weight!')
+    }
   }
 
   return (
@@ -35,15 +69,15 @@ const ProgressScreen = () => {
       }}>
 
         <View style={{ alignItems: 'center' }}>
-          <AppText content={'Goal weight: 100kg'} fontWeight='bold' fontSize={16} color='grey' />
+          <AppText content={`Goal weight: ${goalWeight}`} fontWeight='bold' fontSize={16} color='grey' />
 
           <View style={{ marginTop: 15, }}></View>
           <CircularProgress
-            value={80}
-            maxValue={100}
+            value={currentWeight}
+            maxValue={goalWeight}
             showProgressValue={false}
-            title={100 - 80}
-            subtitle={'MORE KG'}
+            title={goalWeight > currentWeight ? goalWeight - currentWeight : currentWeight - goalWeight}
+            subtitle={goalWeight > currentWeight ? 'MORE KG' : 'OVER GOAL'}
             titleStyle={{ color: 'black', fontFamily: 'monospace', fontSize: 26, }}
             subtitleStyle={{ color: 'black', fontSize: 12, fontFamily: 'monospace' }}
             activeStrokeWidth={4}
@@ -64,7 +98,7 @@ const ProgressScreen = () => {
           paddingVertical: 10,
           marginTop: 20,
         }}>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={() => setIsVisible(true)}>
             <AppText content={'UPDATE WEIGHT'} fontWeight='bold' color={color.PrimaryColor} />
           </TouchableOpacity>
         </View>
@@ -79,13 +113,13 @@ const ProgressScreen = () => {
         paddingTop: 15,
         marginTop: 10,
       }}>
-        <AppText content={'Current weight: '} fontWeight='bold' fontSize={16} color='grey' />
+        <AppText content={`Current weight: ${currentWeight}`} fontWeight='bold' fontSize={16} color='grey' />
         <LineChart
           data={{
-            labels: ["Jan", "Feb", "Mar", "Apr", "May", "June", 'today'],
+            labels: labelChart,
             datasets: [
               {
-                data: data
+                data: dataChart
               }
             ],
           }}
@@ -100,6 +134,41 @@ const ProgressScreen = () => {
           }}
         />
       </View>
+
+      <Modal animationType='fade' transparent={true} visible={isVisible}>
+        <View style={style.modalContent}>
+          <View style={style.titleContainer}>
+            <Pressable onPress={updateWeight}>
+              <MaterialIcons name="check" size={22} color="#fff" />
+            </Pressable>
+
+            <AppText content={'Update Weight'} color="#fff" fontSize={16} />
+
+            <Pressable onPress={() => setIsVisible(false)}>
+              <MaterialIcons name="close" color="#fff" size={22} />
+            </Pressable>
+          </View>
+          <View style={{
+            borderWidth: 1,
+            borderColor: '#fff',
+            marginHorizontal: '25%',
+            borderRadius: 30,
+            padding: 5,
+            marginTop: 20,
+          }}>
+            <TextInput placeholder='Enter your weight'
+              placeholderTextColor={'#fff'}
+              keyboardType={'decimal-pad'}
+              onChangeText={setNewWeight}
+              style={{
+                color: '#fff',
+                fontSize: 16,
+                fontFamily: 'monospace',
+                paddingLeft: 10,
+              }} />
+          </View>
+        </View>
+      </Modal>
     </View>
   )
 }
--- src/styles/tabsStyle.js
+++ src/styles/tabsStyle.js
@@ -8,6 +8,26 @@ const style = StyleSheet.create({
         paddingTop: StatusBar.currentHeight,
         paddingHorizontal: 15,
     },
+    modalContent: {
+        height: '20%',
+        width: '100%',
+        backgroundColor: '#25292e',
+        borderTopRightRadius: 18,
+        borderTopLeftRadius: 18,
+        position: 'absolute',
+        bottom: 0,
+    },
+    titleContainer: {
+        height: '25%',
+        backgroundColor: '#464C55',
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10,
+        paddingHorizontal: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+
 })
 
 export default style
